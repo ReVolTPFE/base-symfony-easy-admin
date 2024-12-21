6e3a2b35600ea4278ccbe57a48afd30c4a7613ab
Arnaud Steiner <arnaudsteiner@orange.fr>
2024-12-21 17:42:16 +0100

    feat: added chartjs chart example

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
